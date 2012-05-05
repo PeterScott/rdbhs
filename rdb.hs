66cc8089f148e96b1ec2abd6e8c8b117cfdd0809
Peter Scott <peter@greplin.com>
2012-05-04 22:42:57 -0700

    Neat little benchmark: sum up value lengths.

--- rdb.hs
+++ rdb.hs
@@ -6,6 +6,24 @@ import Data.Redis.Rdb
 import Control.Monad.IO.Class
 import IO
 
+-- Take things, show them, add newlines, output bytestrings.
 showLines = CL.map (B8.pack . (\x -> show x ++ "\n"))
 
-main = runResourceT $ C.sourceFile "./source.rdb" $= parseRDB $= showLines $$ C.sinkHandle stdout
+-- Print a tedt RDB file.
+printTest :: IO ()
+printTest = runResourceT $ C.sourceFile "./source.rdb" $= parseRDB $= showLines $$ C.sinkHandle stdout
+
+-- Print the sum of all key lengths in a large test file
+printKeylen :: IO ()
+printKeylen = do
+  x <- runResourceT $ C.sourceFile "/Users/peterscott/dump.rdb" $$ keylen
+  print x
+
+-- Sum of all key lengths in an RDB file
+keylen :: Monad m => Sink B8.ByteString m Int
+keylen = parseRDB =$= CL.map klen =$ CL.fold (+) 0
+    where klen (RDBPair (_, _, (RDBString s))) = B8.length s
+          klen _ = 0
+
+-- Behold, the small constant memory usage! Victory is mine!
+main = printKeylen
